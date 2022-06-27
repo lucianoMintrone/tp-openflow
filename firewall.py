d4818dc74ad3129a080fa8f5e5affac09ff56997
None <None>
2022-06-27 09:37:57 -0700

    second rule

--- firewall.py
+++ firewall.py
@@ -10,7 +10,7 @@ from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.revent import *
 from pox.lib.util import dpidToStr
-from pox.lib.addresses import EthAddr
+from pox.lib.addresses import EthAddr, IPAddr
 import pox.lib.packet as pkt
 from collections import namedtuple
 import os
@@ -29,24 +29,43 @@ class Firewall (EventMixin):
 
     def __init__ (self):
         self.listenTo(core.openflow)
-        print("ARRANCA EL Firewall")
         log.debug("Enabling Firewall Module")
 
     def _handle_ConnectionUp (self, event):
-        ''' Add your logic here ... '''
 
-        self.__block_dst_port(port= 80, event= event)
+        ''' First rule: discard all packages that have destination port {port} '''
+        self._discard_dst_port(port= 80, event= event)
+
+        ''' Second rule: discard all packages sent by host 1, dest. port 5001 and UDP '''
+        self._discard_udp_from_host_to_port(host= "10.0.0.1", port= 5001, event= event)
 
         log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
 
-    def __block_dst_port(self, port, event):
-        ''' First rule: discard all packages that have destination port 80 (IPv4, TCP only for now) '''
+    def _discard_dst_port(self, port, event):
 
-        block = of.ofp_match(dl_type = pkt.ethernet.IP_TYPE, nw_proto = pkt.ipv4.TCP_PROTOCOL, tp_dst = port)
+        discard_ipv4_tcp = of.ofp_match(dl_type = pkt.ethernet.IP_TYPE, nw_proto = pkt.ipv4.TCP_PROTOCOL, tp_dst = port)
+        
+        discard_ipv4_udp = of.ofp_match(dl_type = pkt.ethernet.IP_TYPE, nw_proto = pkt.ipv4.UDP_PROTOCOL, tp_dst = port)
+
+
+        for discard_rule in [discard_ipv4_tcp, discard_ipv4_udp]:
+
+            flow_mod = of.ofp_flow_mod()
+            flow_mod.match = discard_rule
+            event.connection.send(flow_mod)
+
+    def _discard_udp_from_host_to_port(self, host, port, event):
+
+        discard_rule = of.ofp_match(
+            dl_type = pkt.ethernet.IP_TYPE, 
+            nw_proto = pkt.ipv4.UDP_PROTOCOL,
+            nw_src = IPAddr(host),
+            tp_dst = port)
 
         flow_mod = of.ofp_flow_mod()
-        flow_mod.match = block
+        flow_mod.match = discard_rule
         event.connection.send(flow_mod)
+
 
 
 def launch ():
