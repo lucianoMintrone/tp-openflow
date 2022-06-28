304edf8454d18f97b138b4fbe42f1921bb7817c8
Luciano Mintrone <luciano@amalgama.co>
2022-06-27 22:02:22 -0300

    Fix hosts ip

--- topo-linear2.py
+++ topo-linear2.py
@@ -19,8 +19,8 @@ class MyTopo( Topo ):
         # Add hosts and switches
         leftHost = self.addHost( 'h1', ip='10.0.0.1', mac='00:00:00:00:00:01' )
         rightHost = self.addHost( 'h2', ip='10.0.0.2', mac='00:00:00:00:00:02' )
-        thirdHost = self.addHost( 'h3', ip='10.0.0.1', mac='00:00:00:00:00:03' )
-        fourthHost = self.addHost( 'h4', ip='10.0.0.2', mac='00:00:00:00:00:04' )
+        thirdHost = self.addHost( 'h3', ip='10.0.0.3', mac='00:00:00:00:00:03' )
+        fourthHost = self.addHost( 'h4', ip='10.0.0.4', mac='00:00:00:00:00:04' )
         switches = []
         for i in range(number_of_switches):
             switches.append( self.addSwitch( f's{i}' ) )
