882df5ecbfc3b2734bfdd093cfca1230811d9cdd
Luciano Mintrone <luciano@amalgama.co>
2022-06-27 21:57:52 -0300

    Add mac address and topo 2

--- topo-linear2.py
+++ topo-linear2.py
@@ -19,12 +19,15 @@ class MyTopo( Topo ):
         # Add hosts and switches
         leftHost = self.addHost( 'h1', ip='10.0.0.1', mac='00:00:00:00:00:01' )
         rightHost = self.addHost( 'h2', ip='10.0.0.2', mac='00:00:00:00:00:02' )
+        thirdHost = self.addHost( 'h1', ip='10.0.0.1', mac='00:00:00:00:00:03' )
+        fourthHost = self.addHost( 'h2', ip='10.0.0.2', mac='00:00:00:00:00:04' )
         switches = []
         for i in range(number_of_switches):
             switches.append( self.addSwitch( f's{i}' ) )
 
         # Add links
         self.addLink( leftHost, switches[0] )
+        self.addLink( thirdHost, switches[0] )
         for i in range(number_of_switches):
             if i == 0:
                 continue
@@ -32,6 +35,7 @@ class MyTopo( Topo ):
             self.addLink( switches[i-1], switches[i] )
 
         self.addLink( switches[-1], rightHost )
+        self.addLink( switches[-1], fourthHost )
 
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
